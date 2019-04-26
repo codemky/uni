585a84010e4789a7f2c0578a6971e05b24135286
chenenru <3326257306@qq.com>
2019-04-26 19:29:44 +0800

    注释好了DisciplineCategoey、Ecomm、Employee、EmpolyeeHistory、FirstLevelDiscipline和LearingDegree类的属性意思和添加了toString、构造器方法

--- src/main/java/edu/uni/userBaseInfo1/bean/AcademicDegree.java
+++ src/main/java/edu/uni/userBaseInfo1/bean/AcademicDegree.java
@@ -1,22 +1,31 @@
 package edu.uni.userBaseInfo1.bean;
 
+/**
+ * 学位表
+ */
 public class AcademicDegree {
+    //学位表id
     private Long id;
 
+    //具体内容
     private String degreeInfo;
 
+    //学位表id的get方法
     public Long getId() {
         return id;
     }
 
+    //学位表id的set方法
     public void setId(Long id) {
         this.id = id;
     }
 
+    //具体内容的get方法
     public String getDegreeInfo() {
         return degreeInfo;
     }
 
+    //具体内容的set方法
     public void setDegreeInfo(String degreeInfo) {
         this.degreeInfo = degreeInfo == null ? null : degreeInfo.trim();
     }
--- src/main/java/edu/uni/userBaseInfo1/bean/Address.java
+++ src/main/java/edu/uni/userBaseInfo1/bean/Address.java
@@ -2,143 +2,168 @@ package edu.uni.userBaseInfo1.bean;
 
 import java.util.Date;
 
+/**
+ * 地址表
+ */
 public class Address {
+    //地址ID
     private Long id;
 
+    //用户ID
     private Long userId;
 
+    //国家 关联到country.id
     private Long country;
 
+    //省份/州  关联到state.id
     private Long state;
 
+    //城市  关联到city.id
     private Long city;
 
+    //县/区  关联到area.id
     private Long area;
 
+    //村/街道  关联到street.id
     private Long street;
 
+    //详细尾缀
     private String detail;
 
+    //邮政编码
     private String zipCode;
 
+    //收件人电话
     private String telephone;
 
+    //0: 当前住址  1: 收件地址
+    //2: 曾经住址  3: 通信地址
+    //4: 办公地址
     private Integer flag;
 
+    //本记录的创建时间
     private Date datetime;
 
+    //本记录的写入者
     private Long byWho;
 
+    //本记录是否有效 0:有效 1:无效
     private Boolean deleted;
 
+    //地址ID的get方法
     public Long getId() {
         return id;
     }
-
+    //地址ID的set方法
     public void setId(Long id) {
         this.id = id;
     }
-
+    //用户ID的get方法
     public Long getUserId() {
         return userId;
     }
-
+    //用户ID的set方法
     public void setUserId(Long userId) {
         this.userId = userId;
     }
-
+    //国家 关联到country.id的get方法
     public Long getCountry() {
         return country;
     }
-
+    //国家 关联到country.id的set方法
     public void setCountry(Long country) {
         this.country = country;
     }
-
+    //省份/州  关联到state.id的get方法
     public Long getState() {
         return state;
     }
-
+    //省份/州  关联到state.id的set方法
     public void setState(Long state) {
         this.state = state;
     }
-
+    //城市  关联到city.id的get方法
     public Long getCity() {
         return city;
     }
-
+    //城市  关联到city.id的set方法
     public void setCity(Long city) {
         this.city = city;
     }
-
+    //县/区  关联到area.id的get方法
     public Long getArea() {
         return area;
     }
-
+    //县/区  关联到area.id的set方法
     public void setArea(Long area) {
         this.area = area;
     }
-
+    //村/街道  关联到street.id的get方法
     public Long getStreet() {
         return street;
     }
-
+    //村/街道  关联到street.id的set方法
     public void setStreet(Long street) {
         this.street = street;
     }
-
+    //详细尾缀的get方法
     public String getDetail() {
         return detail;
     }
-
+    //详细尾缀的set方法
     public void setDetail(String detail) {
         this.detail = detail == null ? null : detail.trim();
     }
-
+    //邮政编码的get方法
     public String getZipCode() {
         return zipCode;
     }
-
+    //邮政编码的set方法
     public void setZipCode(String zipCode) {
         this.zipCode = zipCode == null ? null : zipCode.trim();
     }
-
+    //收件人电话的get方法
     public String getTelephone() {
         return telephone;
     }
-
+    //收件人电话的set方法
     public void setTelephone(String telephone) {
         this.telephone = telephone == null ? null : telephone.trim();
     }
-
+    //0: 当前住址  1: 收件地址
+    //    //2: 曾经住址  3: 通信地址
+    //    //4: 办公地址的get方法
     public Integer getFlag() {
         return flag;
     }
-
+    //0: 当前住址  1: 收件地址
+    //2: 曾经住址  3: 通信地址
+    //4: 办公地址
+    // 的set方法
     public void setFlag(Integer flag) {
         this.flag = flag;
     }
-
+    //本记录的创建时间的get方法
     public Date getDatetime() {
         return datetime;
     }
-
+    //本记录的创建时间的set方法
     public void setDatetime(Date datetime) {
         this.datetime = datetime;
     }
-
+    //本记录的写入者的get方法
     public Long getByWho() {
         return byWho;
     }
-
+    //本记录的写入者的set方法
     public void setByWho(Long byWho) {
         this.byWho = byWho;
     }
-
+    //本记录是否有效 0:有效 1:无效的get方法
     public Boolean getDeleted() {
         return deleted;
     }
-
+    //本记录是否有效 0:有效 1:无效的set方法
     public void setDeleted(Boolean deleted) {
         this.deleted = deleted;
     }
