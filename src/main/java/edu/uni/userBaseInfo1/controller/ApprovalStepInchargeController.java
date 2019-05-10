d46b09a457f01770de29db9ce8daf18172b45d8b
mokuanyuan <384130332@qq.com>
2019-05-10 16:19:16 +0800

    审批模块初步完成

--- src/main/java/edu/uni/userBaseInfo1/controller/ApprovalStepInchargeController.java
+++ src/main/java/edu/uni/userBaseInfo1/controller/ApprovalStepInchargeController.java
@@ -23,11 +23,11 @@ import java.util.List;
 
 /**
  * @Author laizhouhao
- * @Description 关于某种审批的某步骤详情的角色信息模块的Controller层（Http URL请求）的具体实现方法
+ * @Description 关于某审批某步骤详情信息模块的Controller层（Http URL请求）的具体实现方法
  * @Date 16:15 2019/4/29
  **/
 //填写description内容可以在测试模块显示相应的文字和模块
-@Api(description = "某种审批的某步骤详情的角色信息模块")
+@Api(description = "某审批某步骤详情信息模块")
 //Controller类（或者说Http）的请求路径
 //如果添加了路径，则在需要调用该类的方法时需要在方法请求mapping路径前加上类的mapping路径
 @RequestMapping("json/approvalStepIncharge")
@@ -46,7 +46,7 @@ public class ApprovalStepInchargeController {
 
     //内部类，专门用来管理每个get方法所对应缓存的名称。
     static class CacheNameHelper{
-        // ub1_a_approvalStepIncharge_{某种审批的某步骤详情的角色记录id}
+        // ub1_a_approvalStepIncharge_{某审批某步骤详情记录id}
         public static final String Receive_CacheNamePrefix = "ub1_a_approvalStepIncharge_";
         // ub1_a_approvalStepIncharge_listAll
         public static final String ListAll_CacheName = "ub1_a_approvalStepIncharge_listAll";
@@ -56,14 +56,14 @@ public class ApprovalStepInchargeController {
      * Author: laizhouhao 9:55 2019/4/30
      * @param id
      * @return response
-     * @apiNote: 获取某种审批的某步骤详情的角色信息详细
+     * @apiNote: 获取某审批某步骤详情信息详细
      */
     //以下说明为本类中所有方法的注解的解释，仅在本处注释（因为都几乎是一个模版）
     //@ApiOperation：用于在swagger2页面显示方法的提示信息
     //@GetMapping：规定方法的请求路径和方法的请求方式（Get方法）
     //@ApiImplicitParam：用于在swagger2页面测试时用于测试的变量，详细解释可以看Swagger2注解说明
     //@ResponseBody：指明该方法效果等同于通过response对象输出指定格式的数据（JSON）
-    @ApiOperation( value = "以一个id获取一条某种审批的某步骤详情的角色记录详情",notes = "2019年5月6日 18:07:20 已通过测试" )
+    @ApiOperation( value = "以一个id获取一条某审批某步骤详情记录详情",notes = "2019年5月6日 18:07:20 已通过测试" )
     @GetMapping("approvalStepIncharge/{id}")
     @ApiImplicitParam(name = "id", value = "ApprovalStepIncharge表的一个id", required = false, dataType = "Long" , paramType = "path")
     @ResponseBody
@@ -90,7 +90,7 @@ public class ApprovalStepInchargeController {
 
     }
 
-    @ApiOperation( value = "以一个id获取一条某种审批的某步骤详情的角色记录详情",notes = "2019年5月6日 18:07:20 已通过测试" )
+    @ApiOperation( value = "以一个审批规定id获取所有关于该审批规定的步骤详情",notes = "未测试" )
     @GetMapping("approvalStepIncharge/list/{mainId}")
     @ApiImplicitParam(name = "mainId", value = "ApprovalMain表的id", required = true, dataType = "Long" , paramType = "path")
     @ResponseBody
@@ -117,9 +117,9 @@ public class ApprovalStepInchargeController {
 
     /**
      * Author: laizhouhao 16:26 2019/4/29
-     * @apiNote: 查询某种审批的某步骤详情的角色的所有记录
+     * @apiNote: 查询某审批某步骤详情的所有记录
      */
-    @ApiOperation( value = "获取所有某种审批的某步骤详情的角色记录的内容",notes = "2019年5月6日 18:07:47 已通过测试" )
+    @ApiOperation( value = "获取所有某审批某步骤详情记录的内容",notes = "2019年5月6日 18:07:47 已通过测试" )
     @GetMapping("approvalStepIncharges/listAll")
     @ResponseBody
     public void selectAll(HttpServletResponse response)throws Exception{
@@ -138,8 +138,8 @@ public class ApprovalStepInchargeController {
      * @param approvalStepIncharge
      * @return 为某种审批新增一个步骤详情
      */
-    @ApiOperation(value="新增某种审批的某步骤详情的角色信息记录", notes="未测试")
-    @ApiImplicitParam(name = "approvalStepIncharge", value = "某种审批的某步骤详情的角色详情实体", required = true, dataType = "ApprovalStepIncharge")
+    @ApiOperation(value="新增某审批某步骤详情信息记录", notes="未测试")
+    @ApiImplicitParam(name = "approvalStepIncharge", value = "某审批某步骤详情详情实体", required = true, dataType = "ApprovalStepIncharge")
     @PostMapping("/approvalStepIncharge")  //post请求方式
     @ResponseBody
     public Result create(@RequestBody(required = false)ApprovalStepIncharge approvalStepIncharge){
@@ -149,10 +149,10 @@ public class ApprovalStepInchargeController {
             int step = approvalMain.getStepCnt();
             approvalStepIncharge.setStep(step+1);
             approvalMain.setStepCnt(step+1);
-            boolean main_sucess = approvalMainService.update(approvalMain);
+            boolean main_success = approvalMainService.update(approvalMain);
 
             boolean success = approvalStepInchargeService.insert(approvalStepIncharge);
-            if(success || main_sucess){
+            if(success || main_success){
                 cache.delete(ApprovalStepInchargeController.CacheNameHelper.ListAll_CacheName);
                 return Result.build(ResultType.Success);
             }else{
@@ -165,10 +165,10 @@ public class ApprovalStepInchargeController {
     /**
      * Author: laizhouhao 16:47 2019/4/29
      * @param id
-     * @return 删除某种审批的某步骤详情的角色信息结果
+     * @return 删除某审批某步骤详情信息结果
      */
-    @ApiOperation(value="逻辑删除删除某种审批的某步骤详情的角色信息", notes="未测试")
-    @ApiImplicitParam(name = "id", value = "某种审批的某步骤详情的角色id", required = true, dataType = "Long", paramType = "path")
+    @ApiOperation(value="逻辑删除删除某审批某步骤详情信息", notes="未测试")
+    @ApiImplicitParam(name = "id", value = "某审批某步骤详情id", required = true, dataType = "Long", paramType = "path")
     @DeleteMapping("/approvalStepIncharge/{id}")   //delete请求
     @ResponseBody
     public Result destroy(@PathVariable long id){
@@ -187,8 +187,8 @@ public class ApprovalStepInchargeController {
      * @param approvalStepIncharge
      * @return 更新操作结果
      */
-    @ApiOperation(value="更新某种审批的某步骤详情的角色信息", notes="2019年5月6日 18:08:42 已通过测试")
-    @ApiImplicitParam(name = "approvalStepIncharge", value = "某种审批的某步骤详情的角色信息详情实体", required = true, dataType = "ApprovalStepIncharge")
+    @ApiOperation(value="更新某审批某步骤详情信息", notes="2019年5月6日 18:08:42 已通过测试")
+    @ApiImplicitParam(name = "approvalStepIncharge", value = "某审批某步骤详情信息详情实体", required = true, dataType = "ApprovalStepIncharge")
     @PutMapping("/approvalStepIncharge")   //Put请求
     @ResponseBody
     public Result update(@RequestBody(required = false) ApprovalStepIncharge approvalStepIncharge){
